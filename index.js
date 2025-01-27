7c6b9c204c6bf858e504d6a773116e0856b7597a
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2025-01-27 19:08:32 +0500

    version controlled

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
 const mongoose = require('mongoose')
 
 const app = require('./server')
-const port = 3000
+
+
 const MONGO_URL =
   process.env.NODE_ENV === 'production'
     ? 'mongodb://otajonmaxmasoliyev775:gFjQguufGrbS8zoL@undefined/?replicaSet=atlas-q0t0fp-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority'
     : 'mongodb://localhost:27017/greenshop'
-// MongoDB connection
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
 
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
-  console.log('Connected to MongoDB')
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-})
-//
+  .then(() => {
+    console.log('MongoDB ulanishi muvaffaqiyatli amalga oshirildi')
+    app.listen(PORT, () => {
+      console.log(`Server ${PORT}-portda ishlamoqda`)
+    })
+  })
+  .catch(err => console.error('MongoDB ulanishida xatolik yuz berdi:', err))
