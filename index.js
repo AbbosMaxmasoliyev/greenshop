7a4e5a33e5df178421319afabe4e2565acd92ec5
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2025-01-27 18:57:20 +0500

    Pushed for deployed

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose')
 
 const app = require('./server')
 const port = 3000
-
+const MONGO_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'mongodb://otajonmaxmasoliyev775:gFjQguufGrbS8zoL@undefined/?replicaSet=atlas-q0t0fp-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority'
+    : 'mongodb://localhost:27017/greenshop'
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/greenshop', {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
